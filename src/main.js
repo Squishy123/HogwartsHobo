faa95feca0687ad5af0c5831a7f51834985dff62
Christian Wang <christian.gnaw@gmail.com>
2020-04-06 13:22:51 -0400

    Added interactive controls

--- src/main.js
+++ src/main.js
@@ -3,37 +3,82 @@ import Chart from 'chart.js';
 import Game from './sim/sim';
 
 let ctx = document.querySelector('#graph');
+let start = document.querySelector('button');
 
-let game = new Game(60, 3, 5, 10);
-let results = game.runSmart();
+let graph;
 
-let graph = new Chart(ctx, {
-    type: "scatter",
-    data: {
-        datasets: [
-            {
-                label: "Hits",
-                backgroundColor: 'rgb(255, 99, 132)',
-                data: results.hits
-            },
-            {
-                label: "Hobo Position",
-                backgroundColor: 'blue',
-                data: results.hoboPos
-            },
-        ]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 0,
-                    suggestedMax: 10
-                }
-            }]
+start.addEventListener('click', () => {
+    if(graph)
+        graph.destroy();
+
+    startSimulation(Number(document.querySelector('#avg-time-on').value), 
+    Number(document.querySelector('#avg-time-between').value), 
+    Number(document.querySelector('#num-tracks').value));
+})
+
+function startSimulation(avgTimeOnTrack, avgTimesBetween, numTracks) {
+    console.log(numTracks);
+    let game = new Game(60, avgTimeOnTrack, avgTimesBetween, numTracks);
+    let results = game.runSmart();
+
+    let trainPos = [];
+
+    for (let i = 0; i < results.trackPos.length; i++) {
+        for (let j = 0; j < results.trackPos[i].length; j++) {
+            if (!trainPos[j])
+                trainPos.push([]);
+
+            if (results.trackPos[i][j].y == 1)
+                trainPos[j].push({ x: results.trackPos[i][j].x, y: j });
         }
     }
-});
+
+    console.log(trainPos);
+
+    console.log(trainPos.map((tp, i) => ({
+        label: `Track ${i}`,
+        backgroundColor: "green",
+        data: tp
+    })))
+
+    graph = new Chart(ctx, {
+        type: "scatter",
+        data: {
+            datasets: [
+                /*{
+                    label: "Hits",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    data: results.hits
+                },*/
+                {
+                    label: "Hobo Position",
+                    backgroundColor: 'rgba(245, 66, 164, 0.7)',
+                    data: results.hoboPos,
+                    pointRadius: 7
+                },
+                ...trainPos.map((tp, i) => ({
+                    label: `Track ${i}`,
+                    backgroundColor: "rgb(66, 245, 167)",
+                    data: tp,
+                    pointRadius: 10
+                })),
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: numTracks,
+                        stepSize: 1
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
 
 /* MULTIPLE AVERAGE
 let totalAvgs = [];
